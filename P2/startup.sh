a3fff193381b52ddd2a50cc11d0adce4619d4752
Nimra <nimra.aftab@mail.utoronto.ca>
2022-03-03 04:00:13 +0000

    include django project

--- P2/startup.sh
+++ P2/startup.sh
@@ -1,5 +1,6 @@
-python3 -m venv virtenv
-source virtenv/bin/activate
+pip3 install virtualenv
+virtualenv -p `which python3.10` venv
+source venv/bin/activate
 pip install -r requirements.txt
 ./manage.py makemigrations
 ./manage.py migrate
