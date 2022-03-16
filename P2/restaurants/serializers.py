661c4ded683a3fc8a94d9bbf541be0e9364818b8
a <email>
2022-03-15 21:20:53 -0400

    added comments to restaurant serializer

--- P2/restaurants/serializers.py
+++ P2/restaurants/serializers.py
@@ -59,6 +59,11 @@ class ImageSerializer(serializers.ModelSerializer):
         model = RestaurantImage
         fields = ['id', 'img', 'restaurant']
 
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['title', 'text', 'restaurant', 'rating']
+
 
 class RestaurantSerializer(serializers.ModelSerializer):
     owner_id = serializers.ReadOnlyField()
@@ -66,17 +71,14 @@ class RestaurantSerializer(serializers.ModelSerializer):
     num_likes = serializers.IntegerField(source='liked_by.count', read_only=True)
     num_followers = serializers.IntegerField(source='followers.count', read_only=True)
     images = ImageSerializer(many=True, read_only=True)
+    comments = CommentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Restaurant
         fields = ['id', 'owner_id', 'name', 'phone_number', 'address', 'postal_code', 'email', 'logo', 'num_likes',
-                  'num_followers', 'images']
+                  'num_followers', 'images', 'comments']
 
     def create(self, validated_data):
         return super().create(validated_data | {'owner': self.context['request'].user})
 
 
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = ['title', 'text', 'restaurant', 'rating']
